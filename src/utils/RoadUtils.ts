133629ffed625156e2db98af3522498d87bcff7c
MNewbigging <mrnewbigging@gmail.com>
2022-04-20 19:05:15 +0100

    more tidying

--- src/utils/RoadUtils.ts
+++ src/utils/RoadUtils.ts
@@ -1,7 +1,4 @@
 import * as THREE from 'three';
-import { BufferGeometry } from 'three';
-
-import { RoadName } from './ModelLoader';
 
 export class RoadUtils {
   public static copyTransforms(from: THREE.Object3D, to: THREE.Object3D) {
@@ -22,7 +19,7 @@ export class RoadUtils {
     for (let i = 0; i < posCount; i++) {
       const point = new THREE.Vector3().fromBufferAttribute(positions, i);
       const worldPoint = edgePoints.localToWorld(point);
-      points.push(point);
+      points.push(worldPoint);
     }
 
     return points;
@@ -44,62 +41,4 @@ export class RoadUtils {
 
     return closest;
   }
-
-  public static getCorrectLane(
-    roadName: RoadName,
-    travelDir: THREE.Vector3,
-    roadForward: THREE.Vector3,
-    leftLanePoints: THREE.Vector3[],
-    rightLanePoints: THREE.Vector3[]
-  ) {
-    const dot = roadForward.dot(travelDir);
-
-    switch (roadName) {
-      case RoadName.STRAIGHT:
-        return this.getStraightRoadLane(dot, leftLanePoints, rightLanePoints);
-      case RoadName.BEND:
-        return this.getBendRoadLane(dot, leftLanePoints, rightLanePoints);
-    }
-  }
-
-  private static getStraightRoadLane(
-    dot: number,
-    leftLanePoints: THREE.Vector3[],
-    rightLanePoints: THREE.Vector3[]
-  ) {
-    // If dot is 1, same direction as forward; use left lane
-    if (dot === 1) {
-      return leftLanePoints;
-    }
-
-    // If dot is -1, opposite directions; use right lane
-    if (dot === -1) {
-      return rightLanePoints;
-    }
-
-    // In case dot isn't exactly 1 or -1, return something!
-    return leftLanePoints;
-  }
-
-  private static getBendRoadLane(
-    dot: number,
-    leftLanePoints: THREE.Vector3[],
-    rightLanePoints: THREE.Vector3[]
-  ) {
-    // If dot is -1, use left lane (road faces bend entrance at 001, bending to right)
-    if (dot === -1) {
-      return leftLanePoints;
-    }
-
-    // Otherwise, return right lane
-    return rightLanePoints;
-  }
-
-  // Copy this when adding more
-  // private static getBendRoadLane(
-  //   travelDir: THREE.Vector3,
-  //   roadForward: THREE.Vector3,
-  //   leftLanePoints: THREE.Vector3[],
-  //   rightLanePoints: THREE.Vector3[]
-  // ) {}
 }
