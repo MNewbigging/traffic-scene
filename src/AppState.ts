2ecaa040447c480a083978ce6e99e49efe16e799
Matt Newbigging <mrnewbigging@gmail.com>
2022-05-03 20:48:48 +0100

    world clock class, moved delta clock

--- src/AppState.ts
+++ src/AppState.ts
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 import { CanvasListener } from './utils/CanvasListener';
 import { MouseListener } from './utils/MouseListener';
 import { SceneState } from './state/SceneState';
-import { HouseName, ModelLoader, ModelNames, RoadName, VehicleName } from './utils/ModelLoader';
+import { ModelLoader } from './utils/ModelLoader';
+import { WorldClock } from './state/WorldClock';
 
 /**
  * High level state class, handles renderer and main game loop
@@ -12,14 +13,13 @@ export class AppState {
   private canvasListener: CanvasListener;
   private mouseListener = new MouseListener();
   private renderer: THREE.WebGLRenderer;
-  private masterClock = new THREE.Clock();
+  private worldClock = new WorldClock();
   private sceneState: SceneState;
 
   constructor(canvas: HTMLCanvasElement) {
     // Setup screen listener
     this.canvasListener = new CanvasListener(canvas);
     this.canvasListener.addCanvasListener(this.onCanvasResize);
-    document.addEventListener('visibilitychange', this.onVisibilityChange);
 
     // Setup renderer
     this.renderer = new THREE.WebGLRenderer({ canvas });
@@ -45,18 +45,10 @@ export class AppState {
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   };
 
-  private onVisibilityChange = () => {
-    if (document.visibilityState === 'hidden') {
-      this.masterClock.stop();
-    } else {
-      this.masterClock.start();
-    }
-  };
-
   private animate = () => {
     requestAnimationFrame(this.animate);
 
-    const deltaTime = this.masterClock.getDelta();
+    const deltaTime = this.worldClock.delta;
     this.sceneState.updateScene(deltaTime);
 
     this.renderer.render(this.sceneState.scene, this.sceneState.camera);
--- src/state/WorldClock.ts
+++ src/state/WorldClock.ts
@@ -1,7 +1,5 @@
-/**
- * The world clock keeps in-game time, and provides methods for pausing,
- * resuming and fast-forwarding time.
- */
+import * as THREE from 'three';
+
 export enum WorldClockRate {
   PAUSED = 'paused',
   NORMAL = 'normal',
@@ -10,10 +8,32 @@ export enum WorldClockRate {
 
 export class WorldClock {
   public rate = WorldClockRate.NORMAL;
+  public clock = new THREE.Clock();
+  private autoPause = false;
+
+  constructor() {
+    document.addEventListener('visibilitychange', this.onVisibilityChange);
+  }
+
+  public get delta() {
+    return this.clock.getDelta();
+  }
 
   public pause() {}
 
   public resume() {}
 
   public fastForward() {}
+
+  public update(deltaTime: number) {}
+
+  private onVisibilityChange = () => {
+    if (document.visibilityState === 'hidden') {
+      this.clock.stop();
+      this.autoPause = true;
+    } else if (this.autoPause) {
+      this.clock.start();
+      this.autoPause = false;
+    }
+  };
 }
