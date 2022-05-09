7cc57591d276d4c171b206d4300d1caff979b7f8
MNewbigging <mrnewbigging@gmail.com>
2022-05-09 20:17:56 +0100

    notes

--- src/components/info-panels/vehicle-panel/VehiclePanel.tsx
+++ src/components/info-panels/vehicle-panel/VehiclePanel.tsx
@@ -10,6 +10,21 @@ interface VehiclePanelProps {
   onClose: () => void;
 }
 
+/**
+ * UI Panel that shows when:
+ * - selecting a vehicle
+ *
+ * It provides a few buttons:
+ * - Look at - sets vehicle as orbit cam target, selects orbit cam mode
+ * - Follow - selects follow cam mode targeting this vehicle
+ * - Bonnet - selects bonnet cam mode, targeting this vehicle
+ * - Close - closes the window
+ *
+ * The panel should close when:
+ * - pressing Close
+ * - losing focus of the vehicle (double click)
+ * - entering free cam mode
+ */
 export const VehiclePanel: React.FC<VehiclePanelProps> = observer(({ vehicle }) => {
   return (
     <div className={'vehicle-panel'}>
