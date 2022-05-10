005ae32a2abc45c6e0e12c08cd6eb73dafd28eb8
MNewbigging <mrnewbigging@gmail.com>
2022-05-10 21:34:13 +0100

    removed test listener

--- src/AppState.ts
+++ src/AppState.ts
@@ -2,7 +2,6 @@ import { action, makeObservable, observable } from 'mobx';
 
 import { GameState } from './state/GameState';
 import { ModelLoader } from './loaders/ModelLoader';
-import { TestEventType, TestListener } from './state/listeners/TestListener';
 import { UIState } from './state/ui/UIState';
 
 export class AppState {
@@ -13,15 +12,11 @@ export class AppState {
   // TODO - will need a general game loader that loads models, textures, fonts etc
   private modelLoader: ModelLoader;
 
-  private testListener = new TestListener();
-
   constructor() {
     makeObservable(this, {
       loading: observable,
       onAssetsLoaded: action,
     });
-
-    this.testTheListener();
   }
 
   public loadGame() {
@@ -41,20 +36,5 @@ export class AppState {
     // Start game after short delay to allow time for UI to mount
     // TODO - some fade-out of loading screen to cover this period
     setTimeout(() => this.gameState.start(), 10);
-  };
-
-  private testTheListener() {
-    // Subscribe to an event
-    this.testListener.on(TestEventType.TEST_1, this.onEventOne);
-
-    // Fire off event 1
-    this.testListener.fire(TestEventType.TEST_1, { propOne: 'propOne', propTwo: 10 });
-
-    // Fire off event 2
-    this.testListener.fire(TestEventType.TEST_2, { thingA: ['yes', 'no'], thingB: [1, 2, 3] });
-  }
-
-  private onEventOne = (props: { propOne: string; propTwo: number }) => {
-    console.log('got event', props);
   };
 }
